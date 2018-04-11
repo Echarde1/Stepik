b17c0a114305072a7abd9b341b43d0ab35488ddc
Echarde1 <AlucarDqwerty1>
2018-04-11 17:37:55 +0300

    Stairway_Final

--- Algorithms_Chapter_One/Stairway/src/Stairway.java
+++ Algorithms_Chapter_One/Stairway/src/Stairway.java
@@ -6,34 +6,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Stairway {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("/home/danil/Documents/JavaProjects/Stepik/Algorithms_Chapter_One/Stairway/input.txt"));
-        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //BufferedReader reader = new BufferedReader(new FileReader("/home/danil/Documents/JavaProjects/Stepik/Algorithms_Chapter_One/Stairway/input.txt"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine()); //Number of stairs
         String[] temp = reader.readLine().split(" ");
         int[] D = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             D[i] = Integer.parseInt(temp[i - 1]);
         }
+        int[] nominals = D;
 
+        //System.out.println(new Stairway().stairway(D, nominals, n));
+
+    }
+
+    public int stairway(int[] D, int[] nominals,int n) {
         System.out.println(Arrays.toString(D));
 
-        for (int i = 0; i < n; i++) {
-            if (i == n - 1) {
-                D[i + 1] += D[i];
-                break;
-            }
-
-            int oneStairTwice = D[i] + D[i + 1] + D[i + 2];
-            int twoStairs = D[i] + D[i + 2];
-            if (oneStairTwice >= twoStairs) {
-                D[i + 2] = oneStairTwice;
-                i++;
-            } else {
-                D[i + 2] = twoStairs;
-                i++;
-            }
+        for (int i = 2; i <= n; ++i) {
+            D[i] = Math.max(D[i - 1] + nominals[i], D[i - 2] + nominals[i]);
         }
-        System.out.println(D[n]);
+        return D[n];
     }
 }
